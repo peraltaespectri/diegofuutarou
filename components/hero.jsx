c80fb487b4ea15029db2c14d06f2c93672a984a8
peraltaespectri <113333418+peraltaespectri@users.noreply.github.com>
2022-09-20 20:51:02 -0300

    Update hero.jsx

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,10 +1,6 @@
 import Typed from "react-typed";
 import config from "../config";
 export default function Hero() {
-  function AlertIt() {
-    if (window.confirm("open Telegram?"))
-      window.location = `https://t.me/diegofuutarou`;
-  }
   return (
     <div className={"container mt-5"}>
       <h3>
@@ -12,15 +8,14 @@ export default function Hero() {
         <center><code className="text-light">{config.heroText}</code></center>
         <div>
           <span className="badge badge-warning">
-            <center><Typed strings={config.typedArray} typeSpeed={40} loop ={true}</center> />
+            <Typed strings={config.typedArray} typeSpeed={40} loop ={true} />
           </span>
+        </div>
       </h3>
       <button
         className={"btn btn-primary btn-md active mt-4"}
         onClick={() => AlertIt()}
       >
-        Meu Telegram
-      </button>
     </div>
   );
 }
