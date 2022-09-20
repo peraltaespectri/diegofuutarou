d7282f888779bbf0d9c835c5c1bbe0ff29b1e284
peraltaespectri <113333418+peraltaespectri@users.noreply.github.com>
2022-09-20 14:57:58 -0300

    Update header.jsx

--- components/header.jsx
+++ components/header.jsx
@@ -12,8 +12,8 @@ export default function Customhead() {
           <a className={"navbar-brand logo"} >
             <img
               src="./code.png"
-              width="30"
-              height="30"
+              width="0"
+              height="0"
               className="d-inline-block align-top"
               alt="aryanvikash"
             />
