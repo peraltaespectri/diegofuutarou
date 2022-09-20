46257bad730700f683c532c46cf81dd4ee5241a6
peraltaespectri <113333418+peraltaespectri@users.noreply.github.com>
2022-09-20 14:55:17 -0300

    Update header.jsx

--- components/header.jsx
+++ components/header.jsx
@@ -21,9 +21,6 @@ export default function Customhead() {
           </a>
         </Link>
 
-        <Link href="/projects" as="/projects">
-          <a className={"btn btn-outline-light"}>Projetos</a>
-        </Link>
       </nav>
     </div>
   );
