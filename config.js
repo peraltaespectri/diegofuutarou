89be94e88a292ea58db179b418a085ef53ba2c6b
peraltaespectri <113333418+peraltaespectri@users.noreply.github.com>
2022-09-20 15:56:57 -0300

    Update config.js

--- config.js
+++ config.js
@@ -24,7 +24,9 @@ export default {
         {
           name: "API",
           imageUrl: "https://img.icons8.com/color/96/000000/api-settings.png",
-          link: "https://en.wikipedia.org/wiki/API",<br>
+          link: "https://en.wikipedia.org/wiki/API",
+          quebralinha: "<br>",
+
         },
         {
           name: "API",
