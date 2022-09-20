da5164cac310d758c4fcc229486c0438c0f3e1de
peraltaespectri <113333418+peraltaespectri@users.noreply.github.com>
2022-09-20 16:35:04 -0300

    Update header.jsx

--- components/header.jsx
+++ components/header.jsx
@@ -12,12 +12,12 @@ export default function Customhead() {
           <a className={"navbar-brand logo"} >
             <img
               src="./code.png"
-              width="30"
-              height="30"
+              width="0"
+              height="0"
               className="d-inline-block align-top"
               alt="diegofuutarou"
             />
-            <kbd id="username">Diego Fuutarou</kbd>
+            <kbd id="username"></kbd>
           </a>
         </Link>
 
