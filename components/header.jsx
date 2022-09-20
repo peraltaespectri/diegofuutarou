81a864aa18afc0b4df0372c7324073a8a1e08245
peraltaespectri <113333418+peraltaespectri@users.noreply.github.com>
2022-09-20 19:40:14 -0300

    Update header.jsx

--- components/header.jsx
+++ components/header.jsx
@@ -1,6 +1,30 @@
+import config from "../config";
+import Link from "next/link";
+
+
 export default function Customhead() {
+
+
   return (
-    <div id="">
+    <div id="home">
+      <nav className="navbar navbar-light bg-dark">
+        <Link href="/">
+          <a className={"navbar-brand logo"} >
+            <img
+              src="./code.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="Jigar Varma"
+            />
+            <kbd id="username">{config.websiteName}</kbd>
+          </a>
+        </Link>
+
+        <Link href="/projects" as="/projects">
+          <a className={"btn btn-outline-light"}>Projetos</a>
+        </Link>
+      </nav>
     </div>
   );
 }
