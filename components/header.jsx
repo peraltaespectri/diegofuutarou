82bffbc0a9127f7d65afcea46fc928983dce3df8
peraltaespectri <113333418+peraltaespectri@users.noreply.github.com>
2022-09-20 16:41:13 -0300

    Update header.jsx

--- components/header.jsx
+++ components/header.jsx
@@ -5,20 +5,5 @@ export default function Customhead() {
 
 
   return (
-    <div id="home">
-      <nav className="navbar navbar-light bg-dark">
-        <Link href="/">
-          <a className={"navbar-brand logo"} >
-            <img
-              src="./code.png"
-              width="0"
-              height="0"
-              className="d-inline-block align-top"
-              alt=""
-            />
-            <kbd id="username"></kbd>
-         
-      </nav>
-    </div>
   );
 }
