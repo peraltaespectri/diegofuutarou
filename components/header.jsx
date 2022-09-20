378fe10855adda3fbf2fe2ed73f3a902ee04a77f
peraltaespectri <113333418+peraltaespectri@users.noreply.github.com>
2022-09-20 19:38:07 -0300

    Update header.jsx

--- components/header.jsx
+++ components/header.jsx
@@ -1,11 +1,8 @@
 import config from "../config";
 import Link from "next/link";
 
-
-export default function Customhead() {
+{
   return (
-    <div>
-  <p>Qualquer tipo de conteúdo aqui. Como &lt;p&gt;, &lt;table&gt;. Você dá o nome!</p>
-</div>
+    <div></div>
   );
 }
