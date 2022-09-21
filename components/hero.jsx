fd36db6b6f95956fde2a31ea6a76eb39c10ae412
peraltaespectri <113333418+peraltaespectri@users.noreply.github.com>
2022-09-20 23:34:33 -0300

    Update hero.jsx

--- components/hero.jsx
+++ components/hero.jsx
@@ -11,8 +11,8 @@ export default function Hero() {
         <center><kbd className={"text-light"}>{config.heroHead}</kbd></center>
         <center><code className="text-light">{config.heroText}</code></center>
         <div>
-          <center> className="badge badge-warning">
-            <Typed strings={config.typedArray} typeSpeed={40} loop ={true}</center>
+          className="badge badge-warning">
+            <Typed strings={config.typedArray} typeSpeed={40} loop ={true}
         </div>
       </h3>
       <button
