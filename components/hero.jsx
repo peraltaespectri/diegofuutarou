a51458bc6bfc8ed89c49b6b99d5dd1a69f35c80b
peraltaespectri <113333418+peraltaespectri@users.noreply.github.com>
2022-09-21 01:24:27 -0300

    Update hero.jsx

--- components/hero.jsx
+++ components/hero.jsx
@@ -3,15 +3,17 @@ import config from "../config";
 export default function Hero() {
   return (
      <div class="text-center">
-      <h2>
-        <center><kbd className={"p-3 mb-2 bg-info text-white"}>{config.heroHead}</kbd></center>
-        <center><kbd className={"p-3 mb-2 bg-info text-white"}>{config.heroText}</kbd></center>
+      <h3>
+        <center><kbd className={"text-light"}>{config.heroHead}</kbd></center>
+    <div className={"container mt-5"}>
+        </div>
+        <center><code className="text-light">{config.heroText}</code></center>
         <div class="text-center">
           <span className="badge badge-warning">
             <Typed strings={config.typedArray} typeSpeed={40} loop ={true} />
           </span>
         </div>
-      </h2>
+      </h3>
     </div>
   );
 }
