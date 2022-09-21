af54124907dcf60764fa73a0741a75dada7a4fb7
peraltaespectri <113333418+peraltaespectri@users.noreply.github.com>
2022-09-21 00:41:46 -0300

    Update footer.jsx

--- components/footer.jsx
+++ components/footer.jsx
@@ -18,45 +18,6 @@ export default function footer() {
               />
             </a>
           </li>
-
-          <li className="list-inline-item">
-            <a
-              href={"https://t.me/diegofuutarou"}
-              className="btn-floating btn-tw mx-1"
-            >
-              <img
-                style={{ width: "35px", height: "35px" }}
-                src="https://img.icons8.com/fluent/50/000000/telegram-app.png"
-                alt="telegram"
-              />
-            </a>
-          </li>
-
-          <li className="list-inline-item">
-            <a
-              href={"https://www.instagram.com/diegofuutarou"}
-              className="btn-floating btn-tw mx-1"
-            >
-              <img
-                style={{ width: "35px", height: "35px" }}
-                src="https://img.icons8.com/fluent/48/000000/instagram-new.png"
-                alt="Instagram"
-              />
-            </a>
-          </li>
-
-          <li className="list-inline-item">
-            <a
-              href={"https://github.com/diegofuutarou"}
-              className="btn-floating btn-tw mx-1"
-            >
-              <img
-                style={{ width: "35px", height: "35px" }}
-                src="https://img.icons8.com/fluent/344/github.png"
-                alt="github"
-              />
-            </a>
-          </li>
         </ul>
       </footer>
     </div>
