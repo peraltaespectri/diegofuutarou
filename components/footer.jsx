77be02427b21bab5da0a26a63222a3008d5fa8c5
peraltaespectri <113333418+peraltaespectri@users.noreply.github.com>
2022-09-21 00:43:03 -0300

    Update footer.jsx

--- components/footer.jsx
+++ components/footer.jsx
@@ -4,19 +4,6 @@ import Link from "next/link"
 export default function footer() {
   return (
     <div>
-      <footer className="page-footer font-small blue ">
-        <ul className="list-unstyled list-inline text-center">
-          <li className="list-inline-item">
-            <a
-              href={"https://fb.me/diegofuutarou"}
-              className="btn-floating btn-fb mx-1"
-            >
-              <img
-                style={{ width: "35px", height: "35px" }}
-                src={"https://img.icons8.com/fluency/344/facebook-circled.png"}
-                alt="github"
-              />
-            </a>
           </li>
         </ul>
       </footer>
