9a8c0384aa0a6060dec951a4e9309a12323b901d
peraltaespectri <113333418+peraltaespectri@users.noreply.github.com>
2022-09-21 00:43:47 -0300

    Update footer.jsx

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,12 +1,4 @@
 
 import config from "../config"
 import Link from "next/link"
-export default function footer() {
-  return (
-    <div>
-          </li>
-        </ul>
-      </footer>
-    </div>
-  );
-}
+export default function footer()
