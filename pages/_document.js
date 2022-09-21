f379a01cecb9895daf526de1d8bcbc5749d3a7d5
peraltaespectri <113333418+peraltaespectri@users.noreply.github.com>
2022-09-21 01:57:32 -0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,19 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+    static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
   }
 
   render() {
     return (
-      <Html>
-        <Head/>
-        <body className="bg-dark">
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
+    <Html>
+    <Head/>
+    <body className="bg-dark">
+    <Main />
+    <NextScript />
+    </body>
+    </Html>
     );
   }
 }
