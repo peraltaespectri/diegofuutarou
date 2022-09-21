a714941ed4c4ccbb298392647ecb9304eba25a0a
peraltaespectri <113333418+peraltaespectri@users.noreply.github.com>
2022-09-21 00:50:32 -0300

    Update header.jsx

--- components/header.jsx
+++ components/header.jsx
@@ -1,27 +1,9 @@
 import config from "../config";
 import Link from "next/link";
 
-
 export default function Customhead() {
-
-
   return (
-    <div id="home">
-      <nav className="navbar navbar-light bg-dark">
-        <Link href="/">
-          <a className={"navbar-brand logo"} >
-            <img
-              src="./code.png"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              alt="Diego Fuutarou"
-            />
-            <kbd id="username">{config.websiteName}</kbd>
-          </a>
-        </Link>
-
-      </nav>
+    <div id="">
     </div>
   );
 }
