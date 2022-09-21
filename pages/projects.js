7a74e6f62983f498364dcd2508156d55c54afaac
peraltaespectri <113333418+peraltaespectri@users.noreply.github.com>
2022-09-21 01:49:15 -0300

    Update projects.js

--- pages/projects.js
+++ pages/projects.js
@@ -4,38 +4,8 @@ import React, { useState, useEffect } from "react";
 import config from "../config"
 
 export default function projects({projects}) {
-  // const uri = `https://api.github.com/users/peraltaespectri/repos`;
-  //     const [data, setData] = useState({});
-
-      // useEffect(() => {
-      //   axios
-      //     .get(uri)
-      //     .then((res) => {
-      //       setData(res.data);
-      //     })
-      //     .catch((e) => {
-      //       console.log(e.message);
-      //     });
-      // }, [uri]);
-
-
-
-    return(
-
-    <div>
-        <Projects data={projects} />
+  return (
+    <div id="">
     </div>
-
-    );
-}
-
-export async function getStaticProps(context) {
-  const uri = `https://api.github.com/users/peraltaespectri/repos`;
- 
-    const res = await axios.get(uri)
- 
-
-  return {
-    props: {projects:res.data}, // will be passed to the page component as props
-  }
+  );
 }
