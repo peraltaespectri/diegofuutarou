d7ef19a10ab62469c903101b3143ca514ab23ddb
peraltaespectri <113333418+peraltaespectri@users.noreply.github.com>
2022-09-20 23:33:44 -0300

    Update hero.jsx

--- components/hero.jsx
+++ components/hero.jsx
@@ -13,7 +13,6 @@ export default function Hero() {
         <div>
           <center> className="badge badge-warning">
             <Typed strings={config.typedArray} typeSpeed={40} loop ={true}</center>
-          </span>
         </div>
       </h3>
       <button
