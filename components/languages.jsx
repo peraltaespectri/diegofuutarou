bdb61e14731affefadc9a295b6634c12de788d5b
peraltaespectri <113333418+peraltaespectri@users.noreply.github.com>
2022-09-21 01:56:54 -0300

    Update languages.jsx

--- components/languages.jsx
+++ components/languages.jsx
@@ -1,34 +1,34 @@
 import config from "../config";
 
 export default function Languages() {
-  return (
+    return (
     <div className="techstack">
-      {config.stacks.map((stack) => {
-        return (
-          <div key={stack.name}>
-            <div className="lang">
-              <h1>
-                <span className="badge badge-primary">{stack.name}</span>
-              </h1>
-            </div>
+    {config.stacks.map((stack) => {
+    return (
+    <div key={stack.name}>
+    <div className="lang">
+    <h1>
+    <span className="badge badge-primary">{stack.name}</span>
+    </h1>
+    </div>
 
-            <div className="grid-flex">
-              {stack.items.map((item) => {
-                return (
-                  <a
-                    href={item.link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    key={`${stack.name}-${item.name}`}
-                  >
-                    <img src={item.imageUrl} />
-                  </a>
-                );
-              })}
-            </div>
-          </div>
-        );
-      })}
+    <div className="grid-flex">
+    {stack.items.map((item) => {
+    return (
+    <a
+    href={item.link}
+    target="_blank"
+    rel="noopener noreferrer"
+    key={`${stack.name}-${item.name}`}
+    >
+    <img src={item.imageUrl} />
+    </a>
+    );
+    })}
+    </div>
+    </div>
+    );
+    })}
     </div>
   );
 }
