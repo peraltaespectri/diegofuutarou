ce35874eaba82d1567a7771646e025c74e15ad52
peraltaespectri <113333418+peraltaespectri@users.noreply.github.com>
2022-09-21 00:16:26 -0300

    Update hero.jsx

--- components/hero.jsx
+++ components/hero.jsx
@@ -11,7 +11,6 @@ export default function Hero() {
         <center><code className="text-light">{config.heroText}</code></center>
         <center><kbd className={"text-light"}>{config.heroHead}</kbd></center>
         <center><code className="text-light">{config.typedArray}<code></center>
-          </span>
         </div>
       </h3>
     </div>
