7e531cc28777af06f67446fe8c336821278cb910
peraltaespectri <113333418+peraltaespectri@users.noreply.github.com>
2022-09-22 09:47:41 -0300

    Update hero.jsx

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,21 +1,8 @@
-import Typed from "react-typed";
 import config from "../config";
-export default function Hero() {
+import Link from "next/link";
+export default function Customhead() {
   return (
-  <div class="text-center">
-  <h2>
-  <kbd className={"text-light"}>{config.heroHead}</kbd><br />
-  </h2>
-  <div class="text-center">
-  </div>
-  <h3>
-  <code className="text-light">{config.heroText}</code>
-  <div class="text-center">
-  <span className="badge badge-success">
-  <Typed strings={config.typedArray} typeSpeed={40} loop ={true} />
-  </span>
-  </div>
-  </h3>
+  <div id="">
   </div>
   );
 }
