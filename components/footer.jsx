7cb0c193d433e7274a8a267827431e39fef68ffe
peraltaespectri <113333418+peraltaespectri@users.noreply.github.com>
2022-09-22 09:10:59 -0300

    Update footer.jsx

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,4 +1,3 @@
-
 import config from "../config"
 import Link from "next/link"
 export default function footer() {
@@ -6,8 +5,9 @@ export default function footer() {
     <div>
       <footer className="page-footer font-small blue ">
         <ul className="list-unstyled list-inline text-center">
+
       <h2>
-          <code className="badge badge-primary">Minhas redes sociais</code><br/>
+          <code className="badge badge-primary">Minhas Redes Sociais</code><br/>
       </h2>
           <li className="list-inline-item">
              <a
@@ -20,7 +20,7 @@ export default function footer() {
                 alt="facebook"
               />
             </a><br />
-          <code className="text-light">Facebook<br/ >Messenger</code>
+          <code className="text-light">Facebook</code>
           </li>
 
 
@@ -121,6 +121,123 @@ export default function footer() {
             </a><br />
           <code className="text-light">Facebook</code>
           </li>
+
+      <h2>
+          <code className="badge badge-primary">Minhas Redes Sociais</code><br/>
+      </h2>
+          <li className="list-inline-item">
+             <a
+              href={"https://fb.me/" + config.facebookUsername}
+              className="btn-floating btn-fb mx-1"
+            >
+              <img
+                style={{ width: "80px", height: "80px" }}
+                src={"https://img.icons8.com/doodle/48/000000/facebook-new.png"}
+                alt="facebook"
+              />
+            </a><br />
+          <code className="text-light">Facebook</code>
+          </li>
+
+
+          <li className="list-inline-item">
+            <a
+              href={"https://t.me/" + config.tgUsername}
+              className="btn-floating btn-tw mx-1"
+            >
+              <img
+                style={{ width: "80px", height: "80px" }}
+                src="https://img.icons8.com/fluent/50/000000/telegram-app.png"
+                alt="telegram"
+              />
+            </a><br />
+          <code className="text-light">Facebook</code>
+          </li>
+
+          <li className="list-inline-item">
+            <a
+              href={"https://t.me/" + config.tgUsername}
+              className="btn-floating btn-tw mx-1"
+            >
+              <img
+                style={{ width: "80px", height: "80px" }}
+                src="https://img.icons8.com/fluent/50/000000/telegram-app.png"
+                alt="telegram"
+              />
+            </a><br />
+          <code className="text-light">Facebook</code>
+          </li>
+
+          <li className="list-inline-item">
+            <a
+              href={"https://vk.com/" + config.VKUsername}
+              className="btn-floating btn-tw mx-1"
+            >
+              <img
+                style={{ width: "80px", height: "80px" }}
+                src="https://img.icons8.com/fluent/50/000000/telegram-app.png"
+                alt="telegram"
+              />
+            </a><br />
+          <code className="text-light">Facebook</code>
+          </li>
+
+          <li className="list-inline-item">
+            <a
+              href={"https://t.me/" + config.tgUsername}
+              className="btn-floating btn-tw mx-1"
+            >
+              <img
+                style={{ width: "80px", height: "80px" }}
+                src="https://img.icons8.com/fluent/50/000000/telegram-app.png"
+                alt="telegram"
+              />
+            </a><br />
+          <code className="text-light">Facebook</code>
+          </li>
+
+          <li className="list-inline-item">
+            <a
+              href={"https://t.me/" + config.tgUsername}
+              className="btn-floating btn-tw mx-1"
+            >
+              <img
+                style={{ width: "80px", height: "80px" }}
+                src="https://img.icons8.com/fluent/50/000000/telegram-app.png"
+                alt="telegram"
+              />
+            </a><br />
+          <code className="text-light">Facebook</code>
+          </li>
+
+          <li className="list-inline-item">
+            <a
+              href={"https://www.instagram.com/" + config.instaUsername}
+              className="btn-floating btn-tw mx-1"
+            >
+              <img
+                style={{ width: "80px", height: "80px" }}
+                src="https://img.icons8.com/fluent/48/000000/instagram-new.png"
+                alt="Instagram"
+              />
+            </a><br />
+          <code className="text-light">Facebook</code>
+          </li>
+
+          <li className="list-inline-item">
+            <a
+              href={"https://github.com/" + config.githubUsername}
+              className="btn-floating btn-tw mx-1"
+            >
+              <img
+                style={{ width: "80px", height: "80px" }}
+                src="https://img.icons8.com/doodle/48/000000/github.png"
+                alt="github"
+              />
+            </a><br />
+          <code className="text-light">Facebook</code>
+          </li>
+
         </ul>
       </footer>
     </div>
