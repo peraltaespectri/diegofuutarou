7d8ced2520992843f5a09c79700729a4cdaceccc
peraltaespectri <113333418+peraltaespectri@users.noreply.github.com>
2022-09-22 08:28:35 -0300

    Update footer.jsx

--- components/footer.jsx
+++ components/footer.jsx
@@ -7,7 +7,7 @@ export default function footer() {
       <footer className="page-footer font-small blue ">
         <ul className="list-unstyled list-inline text-center">
           <li className="list-inline-item">
-          <code className="text-light">Minhas redes sociais</code><br />
+          <code className="text-light">Minhas redes sociais</code><br /><br />
              <a
               href={"https://fb.me/" + config.facebookUsername}
               className="btn-floating btn-fb mx-1"
