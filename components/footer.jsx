f6e8fc731264284dbc835a04ec8105be74a64347
peraltaespectri <113333418+peraltaespectri@users.noreply.github.com>
2022-09-22 08:22:03 -0300

    Update footer.jsx

--- components/footer.jsx
+++ components/footer.jsx
@@ -7,8 +7,10 @@ export default function footer() {
       <footer className="page-footer font-small blue ">
         <ul className="list-unstyled list-inline text-center">
           <li className="list-inline-item">
-        <div className="badge badge-warning text-center">Minhas Redes Sociais</div><br />
-            <a
+  <div class="text-center">
+  <span className="badge badge-success">
+  <Typed strings={config.typedArray} typeSpeed={40} loop ={true} />
+             <a
               href={"https://fb.me/" + config.facebookUsername}
               className="btn-floating btn-fb mx-1"
             >
