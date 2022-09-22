21492b76d382eb3701871c88c5b5b942b0eb5553
peraltaespectri <113333418+peraltaespectri@users.noreply.github.com>
2022-09-22 09:53:53 -0300

    Update footer.jsx

--- components/footer.jsx
+++ components/footer.jsx
@@ -3,6 +3,24 @@ import config from "../config"
 import Link from "next/link"
 export default function footer() {
   return (
+
+  <div class="text-center">
+  <h2>
+  <kbd className={"text-light"}>{config.heroHead}</kbd><br />
+  </h2>
+  <div class="text-center">
+  </div>
+  <h3>
+  <code className="text-light">{config.heroText}</code>
+  <div class="text-center">
+  <span className="badge badge-success">
+  <Typed strings={config.typedArray} typeSpeed={40} loop ={true} />
+  </span>
+  </div>
+  </h3>
+  </div>
+
+
     <div>
       <footer className="page-footer font-small blue ">
         <ul className="list-unstyled list-inline text-center">
